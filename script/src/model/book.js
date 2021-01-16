ff69356a0b393e20e00463a818f652ff0883f623
lemonsauce21 <lemonsauce21@gmail.com>
2021-01-17 04:47:08 +0900

    도서목록 : 도서 상세보기

--- script/src/model/book.js
+++ script/src/model/book.js
@@ -15,12 +15,23 @@ class BookContainer extends BookAdminBase{
         self : $(`#${parentId}`),
         book : {
           self : 'bookAdmin-book-self',
-          list : 'bookAdmin-book-list'
+          list : 'bookAdmin-book-list',
+          info : 'bookAdmin-book-info'
+        },
+        modal : {
+          bookInfo : {
+            self : 'bookAdmin-modal-bookinfo-self'
+          }
         }
       };
 
       this.view = {
-        bookList : {}
+        bookList : {},
+        bookInfo : {}
+      };
+
+      this.model = {
+        bookInfo : {}
       };
 
       this.bind = () => {
@@ -46,7 +57,7 @@ class BookContainer extends BookAdminBase{
               </table>
             </div>
           </div>
-        
+          <div class="modal fade bd-example-modal-lg" id="${this.id.modal.bookInfo.self}" tabindex='-1'></div>
         `);
       };
 
@@ -98,7 +109,7 @@ class BookContainer extends BookAdminBase{
       ${bookList.reduce((previous, current, currentIndex) => {
         return `
           ${currentIndex > 0 ? previous : ''}
-            <tr>
+            <tr id="${this.id.book.info}-${current._id}">
               <td>#</td>
               <td>${current.title}</td>
               <td>${current.author}</td>
@@ -112,8 +123,21 @@ class BookContainer extends BookAdminBase{
     `;
 
     this.target.book.list = this.target.self.find($(`#${this.id.book.list}`));
-    console.log(this.target.book.list);
     this.target.book.list.html(listHtml);
+    this.target.book.list.off();
+
+    //정보 클릭
+    this.target.self.on('click', `[id^="${this.id.book.info}"]`, async (event) => {
+      let _id = event.currentTarget.id.replace(`${this.id.book.info}-`, "");
+      this.view.bookInfo = _.filter(this.view.bookList, (item)=>{ return item._id === _id });
+      console.log(this.view.bookInfo);
+      //상세정보세팅
+      this.model.bookInfo = new bookInfo(this.id.modal.bookInfo.self);
+      await this.model.bookInfo.initialize(this.view.bookInfo[0]);
+      await this.model.bookInfo.render();
+
+      $(`#${this.id.modal.bookInfo.self}`).modal("show");
+    });
   }
 
 
---
+++ script/src/model/bookInfo.js
@@ -0,0 +1,118 @@
+class bookInfo extends BookAdminBase{
+  constructor(parentId){
+    super();
+
+    try{
+      this.target = {
+        self : $(`#${parentId}`),
+        book : {
+          self: '',
+          list: ''
+        }
+      };
+
+      this.id = {
+        self : $(`#${parentId}`),
+        bookInfo : {
+          self : 'bookAdmin-bookinfo-self',
+          title : 'bookAdmin-bookinfo-title',
+          author : 'bookAdmin-bookinfo-author',
+          publisher : 'bookAdmin-bookinfo-publisher',
+          category1 : 'bookAdmin-bookinfo-category1',
+          category2 : 'bookAdmin-bookinfo-category2',
+          image_url : 'bookAdmin-bookinfo-image_url',
+          price_paper : 'bookAdmin-bookinfo-price_paper',
+          price_ebook : 'bookAdmin-bookinfo-price_ebook',
+          price_sale : 'bookAdmin-bookinfo-price_sale',
+          discount_paper : 'bookAdmin-bookinfo-discount_paper',
+          discount_ebook : 'bookAdmin-bookinfo-discount_ebook',
+          discount_sale : 'bookAdmin-bookinfo-discount_sale'
+        },
+        save : 'bookAdmin-bookinfo-save'
+      };
+
+      this.view = {
+        bookInfo : {}
+      };
+
+      this.bind = () => {
+        this.target.self.html(`
+          <div id="${this.id.self}">
+            <div class="modal-dialog modal-lg">
+              <div class="modal-content">
+                <div class="modal-header" style="text-align: center">
+                  <h3 class="modal-title"><strong>${this.view.bookInfo.title} 상세정보</strong></h3>
+                </div>
+                <div class="modal-body form-horizontal">
+                <!--작업중 : 이미지 추가-->
+                  <div class="form-group">
+                    <label>제목</label><input type="text" class="form-control" id="${this.id.bookInfo.title}" value="${this.view.bookInfo.title}">
+                  </div>
+                  <div class="form-group">
+                    <label>저자</label><input type="text" class="form-control" id="${this.id.bookInfo.author}" value="${this.view.bookInfo.author}">
+                  </div>
+                  <div class="form-group">
+                    <label>출판사</label><input type="text" class="form-control" id="${this.id.bookInfo.publisher}" value="${this.view.bookInfo.publisher}">
+                  </div>
+                  <div class="form-group">
+                    <label>카테고리1</label><input type="text" class="form-control" id="${this.id.bookInfo.category1}" value="${this.view.bookInfo.category1}">
+                  </div>
+                  <div class="form-group">
+                    <label>카테고리2</label><input type="text" class="form-control" id="${this.id.bookInfo.category2}" value="${this.view.bookInfo.category2}">
+                  </div>                
+                  <div class="form-group">
+                    <label>종이책 정가</label><input type="text" class="form-control" id="${this.id.bookInfo.price_paper}" value="${this.view.bookInfo.price_paper}">
+                  </div>
+                  <div class="form-group">
+                    <label>전자책 정가</label><input type="text" class="form-control" id="${this.id.bookInfo.price_ebook}" value="${this.view.bookInfo.price_ebook}">
+                  </div>
+                  <div class="form-group">
+                    <label>판매가</label><input type="text" class="form-control" id="${this.id.bookInfo.price_sale}" value="${this.view.bookInfo.price_sale}">
+                  </div>                             
+                  <div class="form-group">
+                    <label>종이책 할인률</label><input type="text" class="form-control" id="${this.id.bookInfo.discount_paper}" value="${this.view.bookInfo.discount_paper}">
+                  </div>
+                  <div class="form-group">
+                    <label>전자책 할인률</label><input type="text" class="form-control" id="${this.id.bookInfo.discount_ebook}" value="${this.view.bookInfo.discount_ebook}">
+                  </div>
+                  <div class="form-group">
+                    <label>판매가 할인률</label><input type="text" class="form-control" id="${this.id.bookInfo.discount_sale}" value="${this.view.bookInfo.discount_sale}">
+                  </div>
+                </div>
+                <div class="modal-footer">
+                  <button type="button" class="btn btn-sm btn-white"  data-dismiss="modal"><i class="fa fa-upload"></i><span class="bold"> CLOSE</span></button>
+                  <button type="button" class="btn btn-sm btn-info" id="${this.id.save}"><i class="fa fa-upload"></i><span class="bold"> SAVE</span></button>
+                </div>
+              </div>
+            </div>
+          </div>
+        `);
+      };
+
+    }catch(error){
+      throw new Error(error)
+    }
+  };
+
+  async initialize(info){
+    try{
+      this.view.bookInfo = info;
+      super.initialize();
+      return this;
+    }catch(error){
+      throw new Error(error)
+    }
+  }
+
+  async render(){
+    try{
+      this.bind();
+      super.render();
+      return this;
+    }catch(error){
+      throw new Error(error)
+    }
+  }
+
+
+}
